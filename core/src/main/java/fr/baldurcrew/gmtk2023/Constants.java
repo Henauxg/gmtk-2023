575256bd7d913ed5d8b3bfcc54950dc6a8b68e5e
Henauxg <19689618+Henauxg@users.noreply.github.com>
2023-07-09 11:57:40 +0200

    Add DEFAULT_AUDIO_VOLUME to Constants

--- core/src/main/java/fr/baldurcrew/gmtk2023/Constants.java
+++ core/src/main/java/fr/baldurcrew/gmtk2023/Constants.java
@@ -11,4 +11,5 @@ public class Constants {
     public static final float VIEWPORT_HEIGHT = 16;
     public static final float VIEWPORT_WIDTH = 16f / 9f * VIEWPORT_HEIGHT;
     public static final Vector2 TILE_SIZE = new Vector2(1f, 1f);
+    public static final float DEFAULT_AUDIO_VOLUME = 0.2f;
 }
