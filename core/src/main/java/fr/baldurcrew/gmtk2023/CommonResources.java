49fca2f4ef44c6ca2275eec6a80a669c6ba585d0
Henauxg <19689618+Henauxg@users.noreply.github.com>
2023-07-09 17:38:05 +0200

    Switch to a bitmap font since gdx-freetype-teavm still seems to crash for the teavm target

--- core/src/main/java/fr/baldurcrew/gmtk2023/CommonResources.java
+++ core/src/main/java/fr/baldurcrew/gmtk2023/CommonResources.java
@@ -4,22 +4,26 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 
 public class CommonResources {
     private static CommonResources instance;
 
     public final BitmapFont defaultFont;
+
+
     public final Texture greenTileOverlayTexture;
     public final Music mainMenuMusic;
     public final Music mainGameMusic;
 
     public CommonResources() {
-        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/PixeloidSans.ttf"));
-        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        parameter.size = 18;
-        defaultFont = generator.generateFont(parameter);
-        generator.dispose();
+        // Freetype does not appear to work even with gdx-freetype-teavm
+//        FreeTypeFontGenerator generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/PixeloidSans.ttf"));
+//        FreeTypeFontGenerator.FreeTypeFontParameter parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+//        parameter.size = 18;
+//        defaultFont = generator.generateFont(parameter);
+//        generator.dispose();
+
+        defaultFont = new BitmapFont(Gdx.files.internal("fonts/PixeloidSans_36.fnt"));
 
         greenTileOverlayTexture = new Texture("tiles/green_tile_overlay.png");
         mainMenuMusic = Gdx.audio.newMusic(Gdx.files.internal("musics/sinnesloschen-beam.mp3"));
